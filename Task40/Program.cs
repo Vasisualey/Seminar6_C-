9a60ebb18f6822e96f9925af8083e10f21eb1082
Vasisualey <basilnekrasov@mail.ru>
2023-02-21 13:09:56 +0300

    сделал 40 задачу

---
+++ Task40/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход
+// три числа и проверяет, может ли существовать треугольник
+// такой длины
+// теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон
+double a = Input();
+double b = Input();
+double c = Input();
+
+if (CheckTriangle(a,b,c))
+{
+    Console.WriteLine("Такой треугольник существует");
+}
+else
+{
+    Console.WriteLine("Такой треугольник не существует");
+}
+bool CheckTriangle (double a, double b, double c)
+{
+    if((a<(b+c))&&(b<(a+c))&&(c<(a+b))) return true; else return false;
+}
+
+double Input()
+{
+    Console.Write("Введите длину стороны треугольника: ");
+    double res = double.Parse(Console.ReadLine()!);
+    return res;
+}
