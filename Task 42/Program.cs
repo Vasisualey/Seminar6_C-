a42d1d04e3575f8b10a4fd12fe56206626aaf55c
Vasisualey <basilnekrasov@mail.ru>
2023-02-21 18:56:20 +0300

    сделал 42 задачу

---
+++ Task 42/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 42. Напишите программу, которая будет преобрпзовывать 
+// десятичное число в двоичное.
+Console.Clear();
+int num = Input();
+Console.WriteLine(DecimalToBinary(num));
+string DecimalToBinary (int someNum)
+{
+    string str = "";
+    do{
+        str = Convert.ToString(someNum %2) + str;
+        someNum= someNum / 2;
+    } while (someNum != 0);
+    return str;
+}
+int Input()
+{
+    Console.Write("Введите число: ");
+    int res = int.Parse(Console.ReadLine()!);
+    return res;
+}
